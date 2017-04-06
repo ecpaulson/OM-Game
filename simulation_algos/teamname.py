d2c50d69b1390516445b706c13dae5f56b9dd40a
epaulson1 <epaulson1@dhcp-18-111-5-8.dyn.mit.edu>
2017-04-06 10:49:28 -0400

    adding my buyer

--- simulation_algos/teamname.py
+++ simulation_algos/teamname.py
@@ -5,6 +5,10 @@ e.g. awesome_theory_of_om.py
 
 from simulation_game.buyer import Buyer
 from simulation_game.seller import Seller
+from random import randint
+import numpy as np
+import scipy.stats as ss
+import time
 
 # TODO: Replace the string "TeamName" below with your own team's name in camel case. For e.g. "AwesomeTheoryOfOM"
 TEAM_NAME = "TeamName"
@@ -21,11 +25,62 @@ class TeamNameBuyer(Buyer):
         """
         return TEAM_NAME
 
-    def _get_decision_impl(self, t, inventory_h, price_h, reserve_price, b_t_1_n, horizon, num_buyers):
-        """
-        TODO: Fill in your code here -- right now this skeleton code makes the decision to never buy
-        """
-        return 0
+    def trunc_exp_rv(self,low, high, scale, size):
+        rnd_cdf = np.random.uniform(ss.expon.cdf(x=low, scale=scale),
+                                    ss.expon.cdf(x=high, scale=scale),
+                                    size=size)
+        return ss.expon.ppf(q=rnd_cdf, scale=scale)
+
+    def _expected_purchase(self,num_buyers, inventory_h, exp_rate, price_h, n_samples, t):
+        # NEED TO SAMPLE FROM TRUNCATED DISTR
+        # generate sampled reservation prices and put in a n_samples x n_customers array
+        # exp_array = np.random.exponential(2, size=num_buyers[t] * n_samples)
+        if t == 0:
+            exp_array = np.random.exponential(1/(exp_rate*1.0), size=num_buyers * n_samples)
+        else:
+            exp_array = self.trunc_exp_rv(0, price_h[t - 1], 1 / exp_rate, size=num_buyers * n_samples)
+        exp_array_matrix = np.reshape(exp_array, (n_samples, num_buyers))
+        # create booolean array, True if reservation > price
+        above_price = exp_array_matrix > price_h[t]
+        # calculate the number customers with reservation higher than price for that sample (sum the row)
+        num_sales = np.sum(above_price, axis=1)
+        # can only sell as much as remaining inventory
+        truncated_num_sales = np.minimum(num_sales, inventory_h[t])
+        expected_sales = float(sum(truncated_num_sales)) / float(n_samples)
+        return expected_sales
+
+    exp_rate=2
+
+    def _get_decision_impl(self,t, inventory_h, price_h, reserve_price, b_t_1_n, horizon, num_buyers):
+        if reserve_price > price_h[t]:
+            print("res price>price")
+            if t+1==horizon: return 1
+            else:
+                n = randint(0, 2)
+                if n == 0:
+                    alpha = .5
+                elif n == 1:
+                    alpha = .75
+                else:
+                    alpha = 1
+
+                # current price is price_h[t]. Everyone with reservation price > price_h[t-1] has already bought.
+                # Thus we are interested in the expected number of people with reservation price > price_h[t] given
+                # that reservation price < price_h[t-1]
+                # Is this the same as the expected number of buyers with reservation price between price_h[t] and price_h[t-1]?
+                expected_num_purchase = self._expected_purchase(num_buyers, inventory_h, .5, price_h, 100, t)
+                # Get vector of n reservation prices sampled from an exponential
+                # Count number of res prices between price_h[t] and price_h[t-1]
+                # Repeat a ton of times and take average
+
+                est_leftover_inventory = inventory_h[t] - expected_num_purchase
+                est_remaining_buyers = num_buyers - expected_num_purchase
+
+
+                return 1 if est_leftover_inventory > alpha * est_remaining_buyers else 0
+        else:
+            print("res price too low")
+            return 0
 
 
 class TeamNameSeller(Seller):
