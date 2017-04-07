9f01089c2865dd547a8fa3845250677348dc8e7d
epaulson1 <epaulson1@dhcp-18-111-5-8.dyn.mit.edu>
2017-04-07 11:51:38 -0400

    updated simulation file and buyer strategy

--- simulation_algos/teamname.py
+++ simulation_algos/teamname.py
@@ -8,7 +8,13 @@ from simulation_game.seller import Seller
 from random import randint
 import numpy as np
 import scipy.stats as ss
+import scipy.optimize as so
 import time
+import scipy
+import scipy
+from sympy.utilities.lambdify import lambdify
+import sympy
+from sympy import exp
 
 # TODO: Replace the string "TeamName" below with your own team's name in camel case. For e.g. "AwesomeTheoryOfOM"
 TEAM_NAME = "TeamName"
@@ -38,7 +44,7 @@ class TeamNameBuyer(Buyer):
         if t == 0:
             exp_array = np.random.exponential(1/(exp_rate*1.0), size=num_buyers * n_samples)
         else:
-            exp_array = self.trunc_exp_rv(0, price_h[t - 1], 1 / exp_rate, size=num_buyers * n_samples)
+            exp_array = self.trunc_exp_rv(0, price_h[t - 1], 1 / (exp_rate*1.0), size=num_buyers * n_samples)
         exp_array_matrix = np.reshape(exp_array, (n_samples, num_buyers))
         # create booolean array, True if reservation > price
         above_price = exp_array_matrix > price_h[t]
@@ -49,37 +55,49 @@ class TeamNameBuyer(Buyer):
         expected_sales = float(sum(truncated_num_sales)) / float(n_samples)
         return expected_sales
 
-    exp_rate=2
+    def est_rate(self,t, inventory_h, price_h, num_buyers, reserve_price):
+        r = sympy.symbols('r')
+        k = inventory_h[t - 1] - inventory_h[t]
+        for i in range(1, t + 1):
+            k = inventory_h[i - 1] - inventory_h[i]
+            if i == 1:
+                tempfun = -1 * scipy.misc.comb(num_buyers, k) * exp(-r * price_h[0] * k) * (1 - exp(
+                    -r * price_h[0])) ** (num_buyers - k)
+            else:
+                addfun = scipy.misc.comb(num_buyers - (inventory_h[0] - inventory_h[i]), k) * (exp(
+                    -r * price_h[i - 1]) - exp(-r * price_h[i - 2])) ** k * (1 - exp(-r * price_h[i - 1])) ** (
+                num_buyers - (inventory_h[0] - inventory_h[i]))
+                tempfun = tempfun * addfun
+        fun = lambdify(r, tempfun)
+        print(tempfun)
+        res = so.minimize(fun, x0=1/reserve_price, bounds=((0, 10),))
+        return res
+    #exp_rate=2
 
     def _get_decision_impl(self,t, inventory_h, price_h, reserve_price, b_t_1_n, horizon, num_buyers):
-        if reserve_price > price_h[t]:
-            print("res price>price")
-            if t+1==horizon: return 1
-            else:
-                n = randint(0, 2)
-                if n == 0:
-                    alpha = .5
-                elif n == 1:
-                    alpha = .75
-                else:
-                    alpha = 1
-
-                # current price is price_h[t]. Everyone with reservation price > price_h[t-1] has already bought.
-                # Thus we are interested in the expected number of people with reservation price > price_h[t] given
-                # that reservation price < price_h[t-1]
-                # Is this the same as the expected number of buyers with reservation price between price_h[t] and price_h[t-1]?
-                expected_num_purchase = self._expected_purchase(num_buyers, inventory_h, .5, price_h, 100, t)
-                # Get vector of n reservation prices sampled from an exponential
-                # Count number of res prices between price_h[t] and price_h[t-1]
-                # Repeat a ton of times and take average
-
-                est_leftover_inventory = inventory_h[t] - expected_num_purchase
-                est_remaining_buyers = num_buyers - expected_num_purchase
-
-
-                return 1 if est_leftover_inventory > alpha * est_remaining_buyers else 0
+        if t == 0:
+            rate = 1 / reserve_price
         else:
-            print("res price too low")
+            res = self.est_rate(t, inventory_h, price_h, num_buyers, reserve_price)
+            rate = res.x
+        print("est rate", rate)
+        if reserve_price > price_h[t]:
+            if t+1==horizon: return 1
+            elif inventory_h[0]>=num_buyers: return 0
+            else:
+                if inventory_h[0]>=.75*num_buyers:
+                    alpha = .8
+                elif inventory_h[0]>=.5*num_buyers:
+                    alpha = .5
+                else:
+                    alpha = .1
+                expected_num_purchase = self._expected_purchase(num_buyers, inventory_h, rate, price_h, 100, t)
+                current_buyers=num_buyers-(inventory_h[0]-inventory_h[t])
+                est_leftover_inventory = round(inventory_h[t] - expected_num_purchase)
+                est_remaining_buyers = current_buyers - round(expected_num_purchase)
+
+                return 0 if est_leftover_inventory > alpha * est_remaining_buyers else 1
+        else:
             return 0
 
 
